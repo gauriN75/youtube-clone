daa3b2191b70ed778694c7f762940b1a381232c4
Gauri Nalawade <gaurinalawade84@gmail.com>
2023-02-14 13:12:14 +0530

    header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRQLs9SI529AfJgLSLhDkVn9dujscPmvo",
+  authDomain: "clone-fd2b0.firebaseapp.com",
+  projectId: "clone-fd2b0",
+  storageBucket: "clone-fd2b0.appspot.com",
+  messagingSenderId: "843742104466",
+  appId: "1:843742104466:web:77ee8b03274cbcb6030f4b",
+  measurementId: "G-9WXMYVMDP6"
+};
